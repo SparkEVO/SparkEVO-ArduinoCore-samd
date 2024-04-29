d236e132e2fcc061347e0af30a563a1e2b4a5b24
alessandrofrancesconi <alessandrofrancesconi@live.it>
2024-04-30 00:06:01 +0200

    1.1.0: added Time Shot
    fixed bootloaders path

--- bootloaders/zero/build_all_bootloaders.sh
+++ bootloaders/zero/build_all_bootloaders.sh
@@ -5,5 +5,8 @@ BOARD_ID=arduino_zero NAME=samd21_sam_ba make clean all
 BOARD_ID=sparkevo_itronix NAME=samd21_sam_ba_sparkevo_itronix make clean all
 mv -v samd21_sam_ba_sparkevo_itronix.* ../sparkevo_itronix/
 
+BOARD_ID=sparkevo_timeshot2 NAME=samd21_sam_ba_sparkevo_timeshot2 make clean all
+mv -v samd21_sam_ba_sparkevo_timeshot2.* ../sparkevo_timeshot2/
+
 echo Done building bootloaders!
 
