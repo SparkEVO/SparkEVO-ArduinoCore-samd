0a98e0a307fc081a64ea01bef1626b8691f4f279
alessandrofrancesconi <alessandrofrancesconi@live.it>
2024-05-24 22:17:25 +0200

    1.1.7: added first TimeShot2 eeprom

---
+++ eeprom/timeshot2_eeprom.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define EEPROM0_VERSION 1
+#define EEPROM_VERSION  1
+
+typedef struct {
+  uint8_t STRUCT_VERSION;
+  uint8_t DEVICE_TYPE;
+  int DEVICE_SERIAL_NUMBER;
+  int HW_REVISION;
+} __attribute__((packed)) Eeprom0Struct;
+
+inline Eeprom0Struct getDefaultEeprom0()
+{
+  Eeprom0Struct eeprom0;
+  eeprom0.STRUCT_VERSION = EEPROM0_VERSION;
+  eeprom0.DEVICE_TYPE = 0;
+  eeprom0.DEVICE_SERIAL_NUMBER = 0;
+  eeprom0.HW_REVISION = 0;
+  eeprom0.LICENSE_NUMBER = 0;
+  
+  return eeprom0;
+}
